15e50db836ac1728199cdbdcc9b7964b3f61beda
NanthiniMahalingam <nanthini.mahalingam@syncfusion.com>
2023-09-26 18:09:26 +0530

    Updated the sample

--- US_Energy_Consumption/Model/EnergyUtilityProducts.cs
+++ US_Energy_Consumption/Model/EnergyUtilityProducts.cs
@@ -11,16 +11,5 @@ namespace US_Energy_Consumption
         public double NaturalGas { get; set; }
         public double Nuclear { get; set; }
         public double HydroElectric { get; set; }
-
-        public EnergyUtilityProducts(DateTime month,double coal,double petroleumLiquid,double petroleumCoke,double naturalGas,double nuclear, double hydroElectric)
-        {
-            Month = month;
-            Coal = coal;
-            PetroleumLiquid = petroleumLiquid;
-            PetroleumCoke = petroleumCoke;
-            NaturalGas = naturalGas;
-            Nuclear = nuclear;
-            HydroElectric = hydroElectric;
-        }
     }
 }
--- US_Energy_Consumption/ViewModel/EnergyUtilityConsumption.cs
+++ US_Energy_Consumption/ViewModel/EnergyUtilityConsumption.cs
@@ -3,41 +3,22 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Globalization;
 using System.IO;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace US_Energy_Consumption
 {
     public class EnergyUtilityConsumption
     {
-        public ObservableCollection<EnergyUtilityProducts> Coal { get; set; }
-
-        public ObservableCollection<EnergyUtilityProducts> PetroleumLiquid { get; set; }
-
-        public ObservableCollection<EnergyUtilityProducts> PetroleumCoke { get; set; }
-
-        public ObservableCollection<EnergyUtilityProducts> NaturalGas { get; set; }
-
-        public ObservableCollection<EnergyUtilityProducts> Nuclear { get; set; }
-
-        public ObservableCollection<EnergyUtilityProducts> HydroElectric { get; set; }
+        public ObservableCollection<EnergyUtilityProducts> CollectionOfData { get; set; }
 
         public EnergyUtilityConsumption() 
         {
-        
-            Coal = new ObservableCollection<EnergyUtilityProducts>(ReadCSV("US_Energy_Consumption.Resources.us_energy_consumption_data.csv"));
-            PetroleumLiquid = new ObservableCollection<EnergyUtilityProducts>(ReadCSV("US_Energy_Consumption.Resources.us_energy_consumption_data.csv"));
-            PetroleumCoke = new ObservableCollection<EnergyUtilityProducts>(ReadCSV("US_Energy_Consumption.Resources.us_energy_consumption_data.csv"));
-            NaturalGas = new ObservableCollection<EnergyUtilityProducts>(ReadCSV("US_Energy_Consumption.Resources.us_energy_consumption_data.csv"));
-            Nuclear = new ObservableCollection<EnergyUtilityProducts>(ReadCSV("US_Energy_Consumption.Resources.us_energy_consumption_data.csv"));
-            HydroElectric = new ObservableCollection<EnergyUtilityProducts>(ReadCSV("US_Energy_Consumption.Resources.us_energy_consumption_data.csv"));
-            
+            CollectionOfData = new ObservableCollection<EnergyUtilityProducts>();
+            ReadCSV("US_Energy_Consumption.Resources.us_energy_consumption_data.csv");
         }
 
 
-        private IEnumerable<EnergyUtilityProducts> ReadCSV(string resourceStream)
+        private void ReadCSV(string resourceStream)
         {
             Assembly executingAssembly = typeof(App).GetTypeInfo().Assembly;
             Stream inputStream = executingAssembly.GetManifestResourceStream(resourceStream);
@@ -47,15 +28,18 @@ namespace US_Energy_Consumption
             using StreamReader reader = new StreamReader(inputStream);
             while ((line = reader.ReadLine()) != null)
             {
-                lines.Add(line);
+                    lines.Add(line);
             }
+
             lines.RemoveAt(0);
-            return lines.Select(line =>
+            
+            foreach(var dataPoint in lines)
             {
-                string[] data = line.Split(',');
+                string[] data = dataPoint.Split(',');
                 DateTime date = DateTime.ParseExact(data[0], "MMM-yy", CultureInfo.InvariantCulture);
-                return new EnergyUtilityProducts(date, Convert.ToDouble(data[1]), Convert.ToDouble(data[2]), Convert.ToDouble(data[3]), Convert.ToDouble(data[4]), Convert.ToDouble(data[5]), Convert.ToDouble(data[6]));
-            });
+                CollectionOfData.Add(new EnergyUtilityProducts() { Month = date, Coal = Convert.ToDouble(data[1]), PetroleumLiquid = Convert.ToDouble(data[2]), PetroleumCoke = Convert.ToDouble(data[3]), NaturalGas = Convert.ToDouble(data[4]),Nuclear = Convert.ToDouble(data[5]), HydroElectric = Convert.ToDouble(data[6]) });
+              
+            }
         }
     }
 }
